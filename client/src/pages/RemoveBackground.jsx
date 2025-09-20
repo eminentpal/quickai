3f58ce60249a70162b0bf800757e07d880db0cd8
eminentpal <eziechukwudi@gmail.com>
2025-09-20 05:10:35 +0100

    Removebackground and removeobject pages completed

--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -1,9 +1,7 @@
-import { Sparkles } from 'lucide-react';
+import { Eraser, Sparkles } from 'lucide-react';
 import React, { useState } from 'react';
 
 const RemoveBackground = () => {
-  const [selectedCategory, setSelectedCategory] = useState('General');
-
   const [input, setinput] = useState('');
 
   const onSubmitHandler = async (e) => {
@@ -32,21 +30,21 @@ const RemoveBackground = () => {
         <p className="text-xs text-gray-500 font-light mt-1">
           Supports JPG, PNG and other image formats
         </p>
-        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
-          <Hash className="w-5" />
-          Generate Title
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <Eraser className="w-5" />
+          Remove Background
         </button>
       </form>
       {/* Right col */}
       <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 ">
         <div className="flex items-center gap-3">
-          <Hash className="w-5 h-5 text-[#8E37EB]" />
-          <h1 className="text-xl font-semibold">Generated Titles</h1>
+          <Eraser className="w-5 h-5 text-[#FF4938]" />
+          <h1 className="text-xl font-semibold">Processed Image</h1>
         </div>
         <div className="flex-1 flex justify-center items-center">
           <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-            <Hash className="w-9 h-9" />
-            <p>Enter a topic and click "Generate title" to get started </p>
+            <Eraser className="w-9 h-9" />
+            <p>Upload an Image and click "Remove Background" to get started </p>
           </div>
         </div>
       </div>
