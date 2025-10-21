f7a7135f653bb64ba7323b2b9f27b5e2688b496a
eminentpal <eziechukwudi@gmail.com>
2025-10-21 19:22:05 +0100

    Usercontroller partlly done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { getPublishedCreations, getUserCreations, toggleLikeCreation } from "../controllers/userController";
+import { getPublishedCreations, getUserCreations, toggleLikeCreation } from "../controllers/userController.js";
+import { auth } from '../middlewares/auth.js';
 
 const userRouter = express.Router()
 
