cb7748782565099515cc66143a9dbfee815959ad
eminentpal <eziechukwudi@gmail.com>
2025-11-13 13:27:53 +0100

    Community and Dashboard api integrated.

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -5,6 +5,10 @@ import {useAuth} from '@clerk/clerk-react'
 import toast from "react-hot-toast";
 import Markdown from "react-markdown";
 
+ 
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+
 const ReviewResume = () => {
   const [input, setinput] = useState('');
 
